fe54d622b30d3d2af65f75333e159eeb900bfa53
Jason Nutter <jasonn@porch.com>
2018-09-19 14:02:41 -0700

    Add eslint

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,71 +4,71 @@ const path = require('path');
 const assert = require('assert');
 
 exports.sourceNodes = async (
-  { boundActionCreators: { createNode }, createNodeId },
-  {
-    folderPath = '.',
-    gitRepoRoot = '.',
-    gitlab: { domain, projectId, privateToken } = {},
-  }
+    { boundActionCreators: { createNode }, createNodeId },
+    {
+        folderPath = '.',
+        gitRepoRoot = '.',
+        gitlab: { domain, projectId, privateToken } = {}
+    }
 ) => {
-  assert(domain, 'options.gitlab.domain object required');
-  assert(projectId, 'options.gitlab.projectId object required');
-  assert(privateToken, 'options.gitlab.privateKey object required');
+    assert(domain, 'options.gitlab.domain object required');
+    assert(projectId, 'options.gitlab.projectId object required');
+    assert(privateToken, 'options.gitlab.privateKey object required');
 
-  const gitlabReq = request.defaults({
-    baseUrl: domain,
-    json: true,
-    fullResponse: false,
-    headers: {
-      'Private-Token': privateToken,
-    },
-  });
-
-  return dir
-    .promiseFiles(path.resolve(folderPath))
-    .then(files => {
-      const gitRepoRootFullPath = `${path.resolve(gitRepoRoot)}${path.sep}`;
-
-      const relativeFilePaths = files.map(
-        file => `${file.split(gitRepoRootFullPath)[1]}`
-      );
-
-      const promises = relativeFilePaths.map(filePath =>
-        gitlabReq({
-          url: `/api/v4/projects/707/repository/files/${encodeURIComponent(
-            filePath
-          )}`,
-          qs: {
-            ref: 'master',
-          },
-        }).then(({ content, ...file }) =>
-          gitlabReq({
-            url: `/api/v4/projects/707/repository/commits/${
-              file.last_commit_id
-            }`,
-          }).then(latest_commit => ({
-            id: createNodeId(`gitlab-file-${filePath}`),
-            children: [],
-            parent: null,
-            file,
-            latest_commit,
-            internal: {
-              type: 'GitlabFile',
-              content: JSON.stringify({
-                file,
-                latest_commit,
-              }),
-              contentDigest: file.content_sha256,
-            },
-          }))
-        )
-      );
-
-      return Promise.all(promises);
-    })
-    .then(results => {
-      results.forEach(file => {
-        createNode(file);
-      });
+    const gitlabReq = request.defaults({
+        baseUrl: domain,
+        json: true,
+        fullResponse: false,
+        headers: {
+            'Private-Token': privateToken
+        }
     });
+
+    return dir
+        .promiseFiles(path.resolve(folderPath))
+        .then(files => {
+            const gitRepoRootFullPath = `${path.resolve(gitRepoRoot)}${path.sep}`;
+
+            const relativeFilePaths = files.map(
+                file => `${file.split(gitRepoRootFullPath)[1]}`
+            );
+
+            const promises = relativeFilePaths.map(filePath =>
+                gitlabReq({
+                    url: `/api/v4/projects/707/repository/files/${encodeURIComponent(
+                        filePath
+                    )}`,
+                    qs: {
+                        ref: 'master'
+                    }
+                }).then(({ content, ...file }) => // eslint-disable-line no-unused-vars
+                    gitlabReq({
+                        url: `/api/v4/projects/707/repository/commits/${
+                            file.last_commit_id
+                        }`
+                    }).then(latestCommit => ({
+                        id: createNodeId(`gitlab-file-${filePath}`),
+                        children: [],
+                        parent: null,
+                        file,
+                        latestCommit,
+                        internal: {
+                            type: 'GitlabFile',
+                            content: JSON.stringify({
+                                file,
+                                latestCommit
+                            }),
+                            contentDigest: file.content_sha256
+                        }
+                    }))
+                )
+            );
+
+            return Promise.all(promises);
+        })
+        .then(results => {
+            results.forEach(file => {
+                createNode(file);
+            });
+        });
 };
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-exports.loadNodeContent = (node) => Promise.resolve(node.content)
+exports.loadNodeContent = node => Promise.resolve(node.content);
